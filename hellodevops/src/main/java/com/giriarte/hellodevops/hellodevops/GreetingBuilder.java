e208ff3547fddc9e8a7807968a0f27ecc96d11be
giriarte <gaston.iriarte@external.true-pts.com>
2019-03-27 14:44:38 -0300

    Changing something on the code

--- hellodevops/src/main/java/com/giriarte/hellodevops/hellodevops/GreetingBuilder.java
+++ hellodevops/src/main/java/com/giriarte/hellodevops/hellodevops/GreetingBuilder.java
@@ -3,7 +3,7 @@ package com.giriarte.hellodevops.hellodevops;
 public class GreetingBuilder {
 
 	public static String buildGreeting(String name) {
-		return "Hello! " + name;
+		return "Hellooo! " + name;
 	}
 
 }
